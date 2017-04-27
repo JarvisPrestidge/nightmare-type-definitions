caad82fe93121dbc0ccd997eba2dfd8859ae7a6d
Jarvis Prestidge <jarvisprestidge@gmail.com>
2017-04-27 16:17:41 +0100

    Changed type export to module, many other changes

--- nightmare-test.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="nightmare.d.ts" />
-
-import * as Nightmare from 'nightmare';
-
-const nightmare = new Nightmare({
-})
-
---
+++ nightmare-tests.ts
@@ -0,0 +1,23 @@
+
+import * as Nightmare from "nightmare";
+
+var nightmare = new Nightmare({
+    show: true
+})
+
+nightmare
+  .goto('http://yahoo.com')
+  .type('form[action*="/search"] [name=p]', 'github nightmare')
+  .click('form[action*="/search"] [type=submit]')
+  .wait('#main')
+  .evaluate(function () {
+    return document.querySelector('#main .searchCenterMiddle li a').href
+  })
+  .end()
+  .then(function (result) {
+    console.log(result)
+  })
+  .catch(function (error) {
+    console.error('Search failed:', error);
+  });
+
